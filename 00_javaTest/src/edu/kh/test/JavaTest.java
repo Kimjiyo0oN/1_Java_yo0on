89033d04c7dc49b1e5105d0f15312276cf10b691
Kimjiyo0oN <110643229+Kimjiyo0oN@users.noreply.github.com>
2022-08-05 15:25:17 +0900

    내용 수정 22.08.05

--- 00_javaTest/src/edu/kh/test/JavaTest.java
+++ 00_javaTest/src/edu/kh/test/JavaTest.java
@@ -14,6 +14,8 @@ public class JavaTest {
 		//System.out.println("안녕하세요ㅎㅎ");
 		System.out.println("println()은 한 줄 출력 입니다.");
 		System.out.println(100 + 200 + 300 + 400 );
+		
+		System.out.println("git 어렵다");
 	}
 	
 	
